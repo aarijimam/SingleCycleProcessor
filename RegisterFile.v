cfb7470ab0533629861a15812b543278e925e7c8
aarijimam <35100854+aarijimam@users.noreply.github.com>
2023-05-16 09:37:04 +0500

    update

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,5 +1,5 @@
-module RegisterFile(clk, rs,rt,write_reg,RegWrite,write_data,a,b);
-input clk;
+module RegisterFile(clk,reset, rs,rt,write_reg,RegWrite,write_data,a,b);
+input clk,reset;
 input [4:0]rs,rt,write_reg;
 input RegWrite;
 input [31:0]write_data;
@@ -7,7 +7,7 @@ output [31:0]a,b;
 reg [31:0] RegisterFile [7:0];
 
 //Register Files
-initial
+/*initial
 begin
   RegisterFile[0] = 32'b 00000000000000000000000000000001;
   RegisterFile[1] = 32'b 00000000000000000000000000000010;
@@ -18,14 +18,14 @@ begin
   RegisterFile[6] = 32'b 00000000000000000000000000000000;
   RegisterFile[7] = 32'b 00000000000000000000000000000001;
   //RegisterFile[8] = 16'b 0000000000000000;
-end
+end*/
 
 assign a = RegisterFile[rs];
 assign b = RegisterFile[rt];
 
 
 always @ (posedge clk)
-/*begin
+begin
 if(reset == 1)
 begin
   RegisterFile[0] = 32'b 00000000000000000000000000000001;
@@ -37,11 +37,12 @@ begin
   RegisterFile[6] = 32'b 00000000000000000000000000000000;
   RegisterFile[7] = 32'b 00000000000000000000000000000001;
 end
-else*/
+else
 begin
     if(RegWrite)
         RegisterFile[write_reg] = write_data;
     else
         RegisterFile[write_reg] = RegisterFile[write_reg];
+end
 end
 endmodule
